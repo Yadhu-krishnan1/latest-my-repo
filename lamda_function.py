f36c75dd8fbfa471fa93037b2ced355905f0f595
Yadhu-krishnan1 <yadhubepu@gmail.com>
2024-03-05 16:32:35 +0530

    test1

--- lamda_function.py
+++ lamda_function.py
@@ -5,7 +5,7 @@ def lambda_handler(event, context):
     name = event['name']
     
     # Process the input and generate a response
-    greeting = f"Hello, {name}! This is a Lambda function written in Python."
+    greeting = f"Hello, {name}! This is a Lambda function written in Python1."
     
     # Return a response
     return {
